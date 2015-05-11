4ecd04bd7b1b8325d2dd389debdc7daf06c258da
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-05-11 13:55:29 -0500

    Added moduledoc and changed default path to work with Phoenix apps.

--- lib/mix/tasks/exseed.seed.ex
+++ lib/mix/tasks/exseed.seed.ex
@@ -2,6 +2,21 @@ defmodule Mix.Tasks.Exseed.Seed do
   use Mix.Task
   import Mix.Ecto
 
+  @shortdoc "Seed the database."
+  @moduledoc """
+  Seed the database from seed files.
+
+  ## Examples
+
+      mix exseed.seed
+      mix exseed.seed --path path/to/seed/files/
+
+  ## Command line options
+
+    * `--path` - the path to the seed files (defaults to `priv/repo/seeds/`)
+    * `--quiet` - don't display any non-error output.
+  """
+
   def run(args) do
     repo = parse_repo(args)
 
@@ -9,7 +24,7 @@ defmodule Mix.Tasks.Exseed.Seed do
 
     {opts, _, _} = OptionParser.parse args, switches: [quiet: :boolean, path: :string]
 
-    seed_path = opts[:path] || "seeds/"
+    seed_path = opts[:path] || "priv/repo/seeds/"
 
     unless File.exists?(seed_path) do
       raise File.Error, reason: :enoent, action: "find", path: seed_path
