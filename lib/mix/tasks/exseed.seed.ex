ab544e5508439733d1896151f27a187b51dd207b
Stanislav Mekhonoshin <Stanislav.Mekhonoshin@dataart.com>
2016-11-07 16:40:53 +0300

    Load only exs files from seeds directory

--- lib/mix/tasks/exseed.seed.ex
+++ lib/mix/tasks/exseed.seed.ex
@@ -33,9 +33,9 @@ defmodule Mix.Tasks.Exseed.Seed do
         raise File.Error, reason: :enoent, action: "find", path: seed_path
       end
 
-      {:ok, seed_files} = File.ls(seed_path)
+      seed_files = Path.wildcard("#{seed_path}/*.exs")
 
-      seed_files |> Enum.each(&(Code.load_file(Path.join(seed_path, &1))))
+      seed_files |> Enum.each(&(Code.load_file(&1)))
 
       unless opts[:quiet] do
         Mix.shell.info "The database for #{inspect repo} has been seeded."
